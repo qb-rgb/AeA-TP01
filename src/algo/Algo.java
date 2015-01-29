2d2dc0a404218877885eef5f3274c3c01b52e740
qw0te <baert.quentin@gmail.com>
2015-01-29 22:40:09 +0100

    Ajout d'une interface Algo pour representer un algorithme de recherche de motif

---
+++ src/algo/Algo.java
@@ -0,0 +1,19 @@
+package algo;
+
+/**
+ * Interface representant un algorithme de recherche de motif.
+ * 
+ * @author Quentin Baert & Alexandre Verkyndt
+ */
+public interface Algo {
+	
+	/**
+	 * Applique l'algorithme sur une entree et retourne une sortie
+	 * 
+	 * @param entree
+	 * 			entree de l'algorithme
+	 * @return sortie de l'algorithme
+	 */
+	public Sortie apply(Entree entree);
+
+}
