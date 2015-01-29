15caf4b55f9f86aadc79b20deb6fb74730d27d3f
qw0te <baert.quentin@gmail.com>
2015-01-30 00:07:52 +0100

    Ajout du squelette de la classe AlgoKMP

---
+++ src/algo/AlgoKMP.java
@@ -0,0 +1,16 @@
+package algo;
+
+/**
+ * Algorithme de recherche de motif KMP
+ * 
+ * @author Quentin Baert & Alexandre Verkyndt
+ */
+public class AlgoKMP implements Algo {
+
+	@Override
+	public Sortie apply(Entree entree) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
