446c700b544c4b39930d88587b2b73402feed7f0
qw0te <baert.quentin@gmail.com>
2015-01-30 11:15:35 +0100

    Ajout de la methode Entree.equals

--- src/algo/Entree.java
+++ src/algo/Entree.java
@@ -143,4 +143,42 @@ public class Entree {
                 );
     }
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((motif == null) ? 0 : motif.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((sequence == null) ? 0 : sequence.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entree other = (Entree) obj;
+		if (motif == null) {
+			if (other.motif != null)
+				return false;
+		} else if (!motif.equals(other.motif))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (sequence == null) {
+			if (other.sequence != null)
+				return false;
+		} else if (!sequence.equals(other.sequence))
+			return false;
+		return true;
+	}
+
 }
