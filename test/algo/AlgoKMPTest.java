6140d940ef01730da23ebc8a98b3f20270c67931
qw0te <baert.quentin@gmail.com>
2015-02-05 22:54:01 +0100

    Ajoute des tests pour la classe AlgoKMP
    Les tests pour la classe AlgoKMP sont les memes que pour l'AlgoNaif afin
    de pouvoir determiner si les deux algorithmes renvoient les memes
    resultats

--- test/algo/AlgoKMPTest.java
+++ test/algo/AlgoKMPTest.java
@@ -13,7 +13,8 @@ import adn.Motif;
  */
 public class AlgoKMPTest {
 	
-	AlgoKMP algo = AlgoKMP.getInstance();
+	private AlgoKMP algo = AlgoKMP.getInstance();
+	private String sequence = "CACCAGTACGACCGAATG";
 
 	// Verifie si deux tableaux d'entiers sont egaux
 	private boolean checkIntArraysEquality(int[] a1, int[] a2) {
@@ -45,6 +46,101 @@ public class AlgoKMPTest {
 		
 		assertTrue(checkIntArraysEquality(algo.preProcessing(motif1.getMotif()), res1));
 		assertTrue(checkIntArraysEquality(algo.preProcessing(motif2.getMotif()), res2));
+	}
+	
+	@Test
+	public void testAlgoMotifSimplePresent() {
+		Motif motif = new Motif("AC");
+		Entree entree = new Entree("entreeTest", sequence, motif, false, false, false);
+		Sortie sortie = new Sortie(entree);
+		
+		// Ajout des positions attendues
+		sortie.addPosition(1);
+		sortie.addPosition(7);
+		sortie.addPosition(10);
+		
+		assertEquals(sortie, algo.apply(entree));
+	}
+	
+	@Test
+	public void testAlgoMotifSimpleAbsent() {
+		Motif motif = new Motif("TC");
+		Entree entree = new Entree("entreeTest", sequence, motif, false, false, false);
+		Sortie sortie = new Sortie(entree);
+		
+		assertEquals(sortie, algo.apply(entree));
+	}
+	
+	@Test
+	public void testAlgoMotifRev() {
+		Motif motif = new Motif("AC");
+		Entree entree = new Entree("entreeTest", sequence, motif, true, false, false);
+		Sortie sortie = new Sortie(entree);
+		
+		// AC, CA
+		
+		// Ajout des positions attendues
+		sortie.addPosition(0);
+		sortie.addPosition(1);
+		sortie.addPosition(3);
+		sortie.addPosition(7);
+		sortie.addPosition(10);
+		
+		assertEquals(sortie, algo.apply(entree));
+	}
+	
+	@Test
+	public void testAlgoMotifCompl() {
+		Motif motif = new Motif("AC");
+		Entree entree = new Entree("entreeTest", sequence, motif, false, true, false);
+		Sortie sortie = new Sortie(entree);
+		
+		// AC, TG
+		
+		// Ajout des positions attendues
+		sortie.addPosition(1);
+		sortie.addPosition(7);
+		sortie.addPosition(10);
+		sortie.addPosition(16);
+		
+		assertEquals(sortie, algo.apply(entree));
+	}
+	
+	@Test
+	public void testAlgoMotifRevCompl() {
+		Motif motif = new Motif("AC");
+		Entree entree = new Entree("entreeTest", sequence, motif, false, false, true);
+		Sortie sortie = new Sortie(entree);
+		
+		// AC, GT
+		
+		// Ajout des positions attendues
+		sortie.addPosition(1);
+		sortie.addPosition(5);
+		sortie.addPosition(7);
+		sortie.addPosition(10);
+		
+		assertEquals(sortie, algo.apply(entree));
+	}
+	
+	@Test
+	public void testAlgoMotifAll() {
+		Motif motif = new Motif("AC");
+		Entree entree = new Entree("entreeTest", sequence, motif, true, true, true);
+		Sortie sortie = new Sortie(entree);
+		
+		// AC, CA, TG, GT
+		
+		// Ajout des positions attendues
+		sortie.addPosition(0);
+		sortie.addPosition(1);
+		sortie.addPosition(3);
+		sortie.addPosition(5);
+		sortie.addPosition(7);
+		sortie.addPosition(10);
+		sortie.addPosition(16);
+		
+		assertEquals(sortie, algo.apply(entree));
 	}
 
 }
