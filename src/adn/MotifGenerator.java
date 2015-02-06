76fb90ef778744e77330aaeb21d621c3aad510ee
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-06 14:54:01 +0100

    MotifGenerator.java : Correction d'une erreur dans la méthode isLastMotifOfLength().
    La méthode retournait toujours false car la condition présente dans la boucle été toujours vraie.

--- src/adn/MotifGenerator.java
+++ src/adn/MotifGenerator.java
@@ -10,72 +10,72 @@ import java.util.List;
  * @author Quentin Baert & Alexandre Verkyndt
  */
 public class MotifGenerator {
-	
-	///////////////
-	// ATTRIBUTS //
-	///////////////
 
-	private static MotifGenerator instance = new MotifGenerator();
-	
-	/////////////
-	// METHODE //
-	/////////////
-	
-	// Constructeur prive
-	private MotifGenerator () {
-	}
-	
-	/**
-	 * Retourne l'unique instance de la classe MotifGenerator
-	 * 
-	 * @return unique instance de la classe MotifGenerator
-	 */
-	public static MotifGenerator getInstance() {
-		return instance;
-	}
-	
-	// "Incremente" un nucleotide
-	private char incremente(char nucleotide) {
-		if (nucleotide == 'A')
-			return 'C';
-		else if (nucleotide == 'C')
-			return 'G';
-		else if (nucleotide == 'G')
-			return 'T';
-		else
-			return 'A';
-	}
-	
-	// Determine si motif est le dernier motid de taille length
-	private boolean isLastMotifOfLength(String motifStr, int length) {
-		
+    ///////////////
+    // ATTRIBUTS //
+    ///////////////
 
-		for (int i = 0; i < motifStr.length(); i++) {
-			char nucleotide = motifStr.charAt(i);
-			if ((nucleotide != 'T') || (nucleotide != 'U'))
-				return false;
-		}
+    private static MotifGenerator instance = new MotifGenerator();
 
-		return true;
-	}
-	
-	// Retourne le premier motif de taille length
-	private StringBuilder getFirstMotifOfLength(int length) {
-		StringBuilder sb = new StringBuilder();
-		
-		for (int i = 0; i < length; i++) {
-			sb.append('A');
-		}
-		
-		return sb;
-	}
-	
-	// Determine si un nucleotide est le dernier ou non
-	private boolean isLastNucleotide(char nucleotide) {
-		return (nucleotide == 'T') || (nucleotide == 'U');
-	}
+    /////////////
+    // METHODE //
+    /////////////
 
-	/*
+    // Constructeur prive
+    private MotifGenerator () {
+    }
+
+    /**
+     * Retourne l'unique instance de la classe MotifGenerator
+     * 
+     * @return unique instance de la classe MotifGenerator
+     */
+    public static MotifGenerator getInstance() {
+        return instance;
+    }
+
+    // "Incremente" un nucleotide
+    private char incremente(char nucleotide) {
+        if (nucleotide == 'A')
+            return 'C';
+        else if (nucleotide == 'C')
+            return 'G';
+        else if (nucleotide == 'G')
+            return 'T';
+        else
+            return 'A';
+    }
+
+    // Determine si motif est le dernier motid de taille length
+    private boolean isLastMotifOfLength(String motifStr, int length) {
+
+
+        for (int i = 0; i < motifStr.length(); i++) {
+            char nucleotide = motifStr.charAt(i);
+            if (!isLastNucleotide(nucleotide))
+                return false;
+        }
+
+        return true;
+    }
+
+    // Retourne le premier motif de taille length
+    private StringBuilder getFirstMotifOfLength(int length) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < length; i++) {
+            sb.append('A');
+        }
+
+        return sb;
+    }
+
+    // Determine si un nucleotide est le dernier ou non
+    private boolean isLastNucleotide(char nucleotide) {
+        return (nucleotide == 'T') || (nucleotide == 'U');
+    }
+
+    /*
 	private String incremente(String motif) {
 		int length = motif.length();
 		StringBuilder sb = new StringBuilder(length);
