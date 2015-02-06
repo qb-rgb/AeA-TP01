7ca37ce16beb26ad7c187c95979fc318f85f44b2
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-06 14:57:40 +0100

    MotifGenerator.java : Spécification de la méthode getMotifsOfLength().
    La méthode retourne une Liste contenant tous les motifs possible de longueur @length.

--- src/adn/MotifGenerator.java
+++ src/adn/MotifGenerator.java
@@ -97,9 +97,42 @@ public class MotifGenerator {
 	}
 	*/
 
-	public static List<Motif> getMotifsOfLength(int length) {
-		
-		return null;
-	}
+    public List<Motif> getMotifsOfLength(int length) {
+        //Liste qui sera retournee
+        ArrayList<Motif> list_tmp = new ArrayList<Motif>();
+        //recuperation du premier motif
+        StringBuilder motif_tmp = getFirstMotifOfLength(length);
+        //ajout de celui ci dans la liste
+        list_tmp.add(new Motif(motif_tmp.toString()));
+
+        // - ici commence la boucle qui va generer tout les motif possible de longueur @length
+        // - cette ne termine que lorsque le dernier motif a ete genere
+        int i = 0;
+        char nextChar_tmp;
+        char nextChar_tmp2;
+        while(! isLastMotifOfLength(motif_tmp.toString(), length)){
+            i = 0;
+            //on incremente le premier caractere du motif
+            nextChar_tmp = incremente(motif_tmp.charAt(0));
+            
+            //si l'increment genere un A, cela signifie que l'on doit incremente la deuxième lettre au moins
+            if(nextChar_tmp == 'A'){
+                i++;
+                nextChar_tmp2 = incremente(motif_tmp.charAt(i));
+                while(nextChar_tmp2 == 'A' && i<3){
+                    motif_tmp.setCharAt(i, 'A');
+                    i++;
+                    nextChar_tmp2 = incremente(motif_tmp.charAt(i));
+                }
+                motif_tmp.setCharAt(i, nextChar_tmp2);
+            }
+            motif_tmp.setCharAt(0, nextChar_tmp);
+            System.out.println(motif_tmp.toString());
+            list_tmp.add(new Motif(motif_tmp.toString()));
+            
+        }
+
+        return list_tmp;
+    }
 
 }
