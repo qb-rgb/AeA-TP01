205e7a7474a36d14b6fde4513d0def5fdc56eb8d
qw0te <baert.quentin@gmail.com>
2015-02-08 21:29:23 +0100

    Modifie les tests de la classe AlgoNaif
    Les tests de la classe AlgoNaif utilisent maintenant des MotifADN

--- test/algo/AlgoNaifTest.java
+++ test/algo/AlgoNaifTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import adn.Motif;
+import adn.MotifADN;
 import algo.AlgoNaif;
 import algo.Entree;
 import algo.Sortie;
@@ -21,7 +21,7 @@ public class AlgoNaifTest {
 	
 	@Test
 	public void testAlgoMotifSimplePresent() {
-		Motif motif = new Motif("AC");
+		MotifADN motif = new MotifADN("AC");
 		Entree entree = new Entree("entreeTest", sequence, motif, false, false, false);
 		Sortie sortie = new Sortie(entree);
 		
@@ -35,7 +35,7 @@ public class AlgoNaifTest {
 	
 	@Test
 	public void testAlgoMotifSimpleAbsent() {
-		Motif motif = new Motif("TC");
+		MotifADN motif = new MotifADN("TC");
 		Entree entree = new Entree("entreeTest", sequence, motif, false, false, false);
 		Sortie sortie = new Sortie(entree);
 		
@@ -44,7 +44,7 @@ public class AlgoNaifTest {
 	
 	@Test
 	public void testAlgoMotifRev() {
-		Motif motif = new Motif("AC");
+		MotifADN motif = new MotifADN("AC");
 		Entree entree = new Entree("entreeTest", sequence, motif, true, false, false);
 		Sortie sortie = new Sortie(entree);
 		
@@ -62,7 +62,7 @@ public class AlgoNaifTest {
 	
 	@Test
 	public void testAlgoMotifCompl() {
-		Motif motif = new Motif("AC");
+		MotifADN motif = new MotifADN("AC");
 		Entree entree = new Entree("entreeTest", sequence, motif, false, true, false);
 		Sortie sortie = new Sortie(entree);
 		
@@ -79,7 +79,7 @@ public class AlgoNaifTest {
 	
 	@Test
 	public void testAlgoMotifRevCompl() {
-		Motif motif = new Motif("AC");
+		MotifADN motif = new MotifADN("AC");
 		Entree entree = new Entree("entreeTest", sequence, motif, false, false, true);
 		Sortie sortie = new Sortie(entree);
 		
@@ -96,7 +96,7 @@ public class AlgoNaifTest {
 	
 	@Test
 	public void testAlgoMotifAll() {
-		Motif motif = new Motif("AC");
+		MotifADN motif = new MotifADN("AC");
 		Entree entree = new Entree("entreeTest", sequence, motif, true, true, true);
 		Sortie sortie = new Sortie(entree);
 		
