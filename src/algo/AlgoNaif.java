00d34bd65fa78625909418deded01eb68a4ef238
qw0te <baert.quentin@gmail.com>
2015-02-11 21:53:46 +0100

    Ajoute la methode prive AlgoNaif.acceptFor()
    Cette methode etait anciennement dans la classe abstraite Algo.
    Seulement, vu qu'elle est uniquement utilisee dans AlgoNaif, la classe
    Algo est devenue une interface, cette methode est deplacee dans la
    classe AlgoNaif

--- src/algo/AlgoNaif.java
+++ src/algo/AlgoNaif.java
@@ -31,6 +31,22 @@ public class AlgoNaif implements Algo {
 	public static AlgoNaif getInstance() {
 		return instance;
 	}
+	
+	// Determine si un mot est accepte par l'algorithme en fonction d'une entree
+	private boolean acceptFor(String word, Entree entree) {
+		MotifBio motif = entree.getMotif();
+		
+		if (word.equals(motif.getMotif()))
+			return true;
+		else if (entree.takeCompl() && word.equals(motif.getCompl()))
+			return true;
+		else if (entree.takeReverse() && word.equals(motif.getReverse()))
+			return true;
+		else if (entree.takeRevCompl() && word.equals(motif.getRevCompl()))
+			return true;
+		else
+			return false;
+	}
 
 	@Override
 	public Sortie apply(Entree entree) {
